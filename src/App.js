e0846064e4079f17274c9cd425740239e7aadd8b
csandman <csandvik@uvm.edu>
2019-01-21 22:10:32 -0500

    added content editable for textarea for hopefully copying to clipboard on mobile

--- src/App.js
+++ src/App.js
@@ -148,6 +148,7 @@ class App extends Component {
     const el = document.createElement("textarea");
     el.value = str;
     el.setAttribute("readonly", "");
+    el.contentEditable = "true";
     el.style.position = "absolute";
     el.style.left = "-9999px";
     document.body.appendChild(el);
