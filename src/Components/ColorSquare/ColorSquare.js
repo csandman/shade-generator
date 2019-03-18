e81a11c8b65eb753aa3238b25c7a2c1bfb5c998a
csandman <csandvik@uvm.edu>
2019-03-18 01:10:36 -0400

    began swithing to functional components

--- src/Components/ColorSquare/ColorSquare.js
+++ src/Components/ColorSquare/ColorSquare.js
@@ -1,85 +1,74 @@
-import React, { Component } from "react";
+import React from "react";
 import * as clipboard from "clipboard-polyfill";
 import { Tooltip } from "react-tippy";
 import "react-tippy/dist/tippy.css";
 import "./ColorSquare.scss";
 
-class ColorSquare extends Component {
-  constructor(props) {
-    super(props);
-    this.copyHexCode = this.copyHexCode.bind(this);
-    this.copyRgb = this.copyRgb.bind(this);
-  }
-
-  copyHexCode(e) {
-    const output = this.props.color.hex;
+const ColorSquare = props => {
+  const copyHexCode = e => {
+    const output = props.color.hex;
     clipboard.writeText(output);
-    this.changeButtonText(e.target, "Copied!");
-  }
+    changeButtonText(e.target, "Copied!");
+  };
 
-  copyRgb(e) {
-    const rgb = this.props.color.rgb;
+  const copyRgb = e => {
+    const rgb = props.color.rgb;
     const output = "rgb(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ")";
     clipboard.writeText(output);
-    this.changeButtonText(e.target, "Copied!");
-  }
+    changeButtonText(e.target, "Copied!");
+  };
 
-  changeButtonText(button, text) {
+  const changeButtonText = (button, text) => {
     const original = button.textContent;
     button.textContent = text;
     setTimeout(() => {
       button.textContent = original;
     }, 1200);
-  }
+  };
 
-  render() {
-    return (
-      <div className="color-square">
-        <Tooltip
-          trigger="mouseenter"
-          position="bottom"
-          arrow={true}
-          animation="fade"
-          interactive
-          theme="dark-border"
-          duration={200}
-          html={
-            <div>
-              <div className="tooltip-title">CLICK TO COPY</div>
-              <div className="popup-button">
-                <button onClick={this.copyHexCode}>
-                  {this.props.color.hex.toUpperCase()}
-                </button>
-              </div>
-              <div className="popup-button">
-                <button onClick={this.copyRgb}>
-                  {"rgb: (" +
-                    this.props.color.rgb[0] +
-                    ", " +
-                    this.props.color.rgb[1] +
-                    ", " +
-                    this.props.color.rgb[2] +
-                    ")"}
-                </button>
-              </div>
+  return (
+    <div className="color-square">
+      <Tooltip
+        trigger="mouseenter"
+        position="bottom"
+        arrow={true}
+        animation="fade"
+        interactive
+        theme="dark-border"
+        duration={200}
+        html={
+          <div>
+            <div className="tooltip-title">CLICK TO COPY</div>
+            <div className="popup-button">
+              <button onClick={copyHexCode}>
+                {props.color.hex.toUpperCase()}
+              </button>
             </div>
-          }
-        >
-          <div
-            style={{ backgroundColor: this.props.color.hex }}
-            className="color-tile"
-            id={"tippy-tooltip-" + this.props.squareNumber}
-            onClick={() => {
-              this.props.handleColorClick(
-                this.props.color.hex,
-                this.props.colorDataNumber
-              );
-            }}
-          />
-        </Tooltip>
-      </div>
-    );
-  }
-}
+            <div className="popup-button">
+              <button onClick={copyRgb}>
+                {"rgb: (" +
+                  props.color.rgb[0] +
+                  ", " +
+                  props.color.rgb[1] +
+                  ", " +
+                  props.color.rgb[2] +
+                  ")"}
+              </button>
+            </div>
+          </div>
+        }
+      >
+        <div
+          style={{ backgroundColor: props.color.hex }}
+          className="color-tile"
+          id={"tippy-tooltip-" + props.squareNumber}
+          onClick={() => {
+            props.handleColorClick(props.color.hex, props.colorDataNumber);
+          }}
+        />
+      </Tooltip>
+    </div>
+  );
+};
 
 export default ColorSquare;
