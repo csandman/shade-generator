95fe5a908fd36205cc4f4299352c040035dbb949
csandman <csandvik@uvm.edu>
2019-04-09 23:17:21 -0400

    Started working on help menu

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -114,7 +114,7 @@ const Sidebar = props => {
           </div>
           <div className="main-menu-item" onClick={openHelpMenu}>
             <i className="icon fas fa-question-circle" />
-            <span>Help</span>
+            <span>What is this?</span>
           </div>
           <div className="footer-row">
             <a
@@ -284,76 +284,86 @@ const Sidebar = props => {
         >
           <div onClick={closeSubMenu} className="sub-menu-header">
             <i className="icon fas fa-arrow-left" />
-            <span>Help</span>
+            <span>What is this?</span>
           </div>
 
           <div className="sub-menu-content">
             <section className="help-menu-content">
-              <h4>What is this?</h4>
+              <h4>Welcome to Shade Generator!</h4>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
+                Shade generator is a project that started as a codepen a couple
+                years ago. The idea behind it was simple, when you overlap a
+                color with a transparent white or black section, you end up with
+                a tint or shade of that color. As many people do in web
+                development, I would often use this trick to create a
+                contrasting color effect for button hovers or element borders.
+                This got annoying because in order to make the effect work, I
+                would need two elements on the screen for the overlap where I
+                often only needed one. So, I decided to make a little tool to
+                help me find the color codes for this resulting shade by giving
+                me a variety of overlapping icons with the color I was plabbibg
+                on using as the background. However it was still not the most
+                convenient because I had to use a chrome plugin called
+                ColorZilla to get the actual code of the resulting color,
+                however I could not think of a better way. Finally, after using
+                this tool hundreds of times I finally thought, there must be
+                some way to calculate this resulting color!
               </p>
-              <h4>What is this?</h4>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
+                After a quick google search, I found{" "}
+                <a
+                  href="https://www.viget.com/articles/equating-color-and-transparency/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  this link
+                </a>
+                . I immediately realized calculating the colors I wanted was
+                possible with this very simple formula...
               </p>
-              <h4>What is this?</h4>
+              <h4>Features</h4>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
+                Throughout the process of making this website, I slowly started
+                adding in some cool additional features. Some served a purpose,
+                some just for fun.
               </p>
-              <h4>What is this?</h4>
+              <h5>Color Parser</h5>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
+                One of the first things I added was the NPM package{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.npmjs.com/package/parse-color"
+                >
+                  parse-color
+                </a>
+                . It takes a string and parses it for its color values in many
+                different formats. It can parse colors from hex values, rgb
+                values, css named colors, and cymk formats. The result is an
+                object that has the following components:
+                <ul>
+                  <li>rgb - an array of [ red, green, blue ]</li>
+                  <li>hsl - an array of [ hue, saturation, luminosity ]</li>
+                  <li>hsv - an array of [ hue, saturation, value ]</li>
+                  <li>cmyk - an array of [ cyan, magenta, yellow, blac(k) ]</li>
+                  <li>keyword - the name of the color, if known</li>
+                  <li>hex - the hex rgb string #rrggbb</li>
+                  <li>rgba - rgb plus an alpha value</li>
+                  <li>hsla - hsl plus an alpha value</li>
+                  <li>hsva - hsv plus an alpha value</li>
+                  <li>
+                    cmyka - cmyk plus an alpha value
+                  </li>
+                </ul>
+                This was useful, not only to interpret input from the user but
+                also for providing this app with hex and rgb values, both of
+                which are important to its functionality.
               </p>
-              <h4>What is this?</h4>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
+              <h5>Color Names</h5>
+              <h5>Offline Functionality</h5>
+              <h5>Random Colors</h5>
+              <h5>Split Screen</h5>
+              <h5>Color Copying</h5>
             </section>
           </div>
         </div>
