8af1e6432a20450a4c9727b5c2f20bae5f7fcd7e
csandman <csandvik@uvm.edu>
2020-01-21 17:39:24 -0500

    added local version of Nunito typeface

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-nunito';
 import './index.css';
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
