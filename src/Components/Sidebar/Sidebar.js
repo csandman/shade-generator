f7856795c8e234d0e85f7096ff9d80ce7030ef28
csandman <csandvik@uvm.edu>
2020-01-21 17:39:04 -0500

    Added history menu into offline mode

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -61,24 +61,22 @@ const Sidebar = ({ handleColorClick = () => {} }) => {
             menuStates.isMainMenuOpen ? '' : ' hidden'
           }`}
         >
+          <div
+            className="main-menu-item"
+            id="HistoryMenu"
+            onClick={e => openMenu(e.currentTarget.id)}
+          >
+            <i className="icon fas fa-history" />
+            <span>History</span>
+          </div>
           {online && (
-            <div className="online-menu-items">
-              <div
-                className="main-menu-item"
-                id="HistoryMenu"
-                onClick={e => openMenu(e.currentTarget.id)}
-              >
-                <i className="icon fas fa-history" />
-                <span>History</span>
-              </div>
-              <div
-                className="main-menu-item"
-                id="TopColorsMenu"
-                onClick={e => openMenu(e.currentTarget.id)}
-              >
-                <i className="icon fas fa-award" />
-                <span>Top Colors</span>
-              </div>
+            <div
+              className="main-menu-item"
+              id="TopColorsMenu"
+              onClick={e => openMenu(e.currentTarget.id)}
+            >
+              <i className="icon fas fa-award" />
+              <span>Top Colors</span>
             </div>
           )}
           <div
