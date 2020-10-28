28820aaa89d7e53eea2fafdb458bb6836692c54a
csandman <chris.sandvik@gmail.com>
2020-10-28 12:11:55 -0400

    refactor App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useContext,
+  useRef,
+  useCallback,
+} from 'react';
 import { useOnline } from 'react-browser-hooks';
 import ReactGA from 'react-ga';
 import Header from '../Header';
@@ -21,7 +27,7 @@ import {
 } from '../../Functions';
 
 // returns [ hex1, hex2, isSplitScreen ]
-function parseURL() {
+const parseURL = () => {
   const path = window.location.pathname.slice(1);
   if (path.length) {
     const splitUrl = window.location.pathname.slice(1).toUpperCase().split('-');
@@ -40,7 +46,7 @@ function parseURL() {
   }
 
   return ['', '', false];
-}
+};
 
 const [initialHex1, initialHex2, initialSplitState] = parseURL();
 
@@ -52,15 +58,17 @@ const initialColor2 = getAllColorInfo(
   initialHex2.length ? attemptCreateColor(initialHex2) : getRandomColor()
 );
 
-let popCount = 2;
-
 const App = () => {
   const online = useOnline();
   const [colorData1, setColorData1] = useState(initialColor1);
   const [colorData2, setColorData2] = useState(initialColor2);
+  const hex1 = colorData1.hex;
+  const hex2 = colorData2.hex;
   const [loading, setLoading] = useState(true);
   const [curInputVal1, setCurInputVal1] = useState(initialHex1);
   const [curInputVal2, setCurInputVal2] = useState(initialHex2);
+
+  const popCount = useRef(2);
 
   const { splitView, splitViewDisabled, setSplitView } = useContext(
     SplitViewContext
@@ -68,6 +76,87 @@ const App = () => {
 
   const { firebase } = useContext(FirebaseContext);
 
+  const { updateRecentColors } = useContext(HistoryContext);
+
+  const updateStateValues = useCallback(
+    (color, colorNum) => {
+      const addMenuItem = (newColor) => {
+        const colorToAdd = { ...newColor };
+        colorToAdd.timeAdded = new Date();
+        colorToAdd.timeString = new Date().toLocaleTimeString([], {
+          hour: 'numeric',
+          minute: 'numeric',
+        });
+        colorToAdd.dateString = new Date().toLocaleDateString();
+
+        updateRecentColors(colorToAdd);
+
+        if (online) {
+          const colorRef = firebase.db
+            .collection('color-history')
+            .doc(colorToAdd.hex);
+
+          colorRef.get().then((colorRecord) => {
+            if (colorRecord.exists) {
+              colorToAdd.count = (colorRecord.data().count || 0) + 1;
+              colorRef.update(colorToAdd);
+            } else {
+              colorToAdd.count = 1;
+              colorRef.set(colorToAdd);
+            }
+          });
+        }
+      };
+
+      let colorData;
+      if (typeof color === 'object') {
+        if (color?.shades?.[0]?.contrastRatio) {
+          colorData = color;
+        } else {
+          colorData = getAllColorInfo(color.hex);
+        }
+      } else if (typeof color === 'string') {
+        colorData = getAllColorInfo(color);
+      } else {
+        return;
+      }
+
+      delete colorData.keyword;
+      if (colorNum === 1) {
+        setColorData1(colorData);
+        setCurInputVal1(colorData.hex);
+      } else if (colorNum === 2) {
+        setColorData2(colorData);
+        setCurInputVal2(colorData.hex);
+      }
+
+      addMenuItem({ ...colorData });
+    },
+    [firebase.db, online, updateRecentColors]
+  );
+
+  const getRandomColors = () => {
+    ReactGA.event({
+      category: 'Button Press',
+      action: 'Random color button',
+    });
+    const randomColor1 = getAllColorInfo(getRandomColor());
+    updateStateValues(randomColor1, 1);
+
+    if (splitView && !splitViewDisabled) {
+      const randomColor2 = getAllColorInfo(getRandomColor());
+      updateStateValues(randomColor2, 2);
+    }
+  };
+
+  const handleSubmit = (inputNum, inputVal) => {
+    const hex = parseColorFromString(inputVal);
+    if (hex) {
+      updateStateValues(hex, inputNum);
+    }
+  };
+
+  // initialize app
   useEffect(() => {
     if (online) {
       ReactGA.initialize(process.env.REACT_APP_GA_CODE);
@@ -84,109 +173,27 @@ const App = () => {
     // setTimeout(() => setLoading(false), 1000);
   }, [online, setSplitView]);
 
-  const hex1 = colorData1.hex;
-  const hex2 = colorData2.hex;
-
   // Update url when colors change or when split view
   useEffect(() => {
-    if (!popCount) {
-      if (splitView === true && !splitViewDisabled) {
-        window.history.pushState(
-          { hex1, hex2 },
-          'Shade Generator',
-          `${hex1.slice(1)}-${hex2.slice(1)}`
-        );
-      } else {
-        window.history.pushState({ hex1 }, 'Shade Generator', hex1.slice(1));
+    if (!popCount.current) {
+      const newState = { hex1 };
+      let newPath = hex1.slice(1);
+      if (splitView && !splitViewDisabled) {
+        newState.hex2 = hex2;
+        newPath += `-${hex2.slice(1)}`;
       }
+      window.history.pushState(newState, 'Shade Generator', newPath);
     } else {
-      popCount -= 1;
+      popCount.current -= 1;
     }
   }, [hex1, hex2, splitView, splitViewDisabled]);
 
-  const { updateRecentColors } = useContext(HistoryContext);
-
-  function addMenuItem(newColor) {
-    const colorToAdd = { ...newColor };
-    colorToAdd.timeAdded = new Date();
-    colorToAdd.timeString = new Date().toLocaleTimeString([], {
-      hour: 'numeric',
-      minute: 'numeric',
-    });
-    colorToAdd.dateString = new Date().toLocaleDateString();
-
-    updateRecentColors(colorToAdd);
-
-    if (online) {
-      const colorRef = firebase.db
-        .collection('color-history')
-        .doc(colorToAdd.hex);
-
-      colorRef.get().then((colorRecord) => {
-        if (colorRecord.exists) {
-          colorToAdd.count = (colorRecord.data().count || 0) + 1;
-          colorRef.update(colorToAdd);
-        } else {
-          colorToAdd.count = 1;
-          colorRef.set(colorToAdd);
-        }
-      });
-    }
-  }
-
-  function updateStateValues(color, colorNum) {
-    let colorData;
-    if (typeof color === 'object') {
-      if (color.shades && color.shades[0].contrastRatio) {
-        colorData = color;
-      } else {
-        colorData = getAllColorInfo(color.hex);
-      }
-    } else if (typeof color === 'string') {
-      colorData = getAllColorInfo(color);
-    } else {
-      return;
-    }
-
-    delete colorData.keyword;
-    if (colorNum === 1) {
-      setColorData1(colorData);
-      setCurInputVal1(colorData.hex);
-    } else if (colorNum === 2) {
-      setColorData2(colorData);
-      setCurInputVal2(colorData.hex);
-    }
-
-    addMenuItem({ ...colorData });
-  }
-
-  function getRandomColors() {
-    ReactGA.event({
-      category: 'Button Press',
-      action: 'Random color button',
-    });
-    const randomColor1 = getAllColorInfo(getRandomColor());
-    updateStateValues(randomColor1, 1);
-
-    if (splitView && splitViewDisabled === false) {
-      const randomColor2 = getAllColorInfo(getRandomColor());
-      updateStateValues(randomColor2, 2);
-    }
-  }
-
-  function handleSubmit(inputNum, inputVal) {
-    const hex = parseColorFromString(inputVal);
-    if (!hex) {
-      return;
-    }
-    updateStateValues(hex, inputNum);
-  }
-
+  // update color states based on previous url
   useEffect(() => {
-    window.onpopstate = (e) => {
-      popCount = 1;
+    const handlePopState = (e) => {
+      popCount.current = 1;
       if (e?.state?.hex2) {
-        popCount = 2;
+        popCount.current = 2;
       }
 
       updateStateValues(e?.state?.hex1, 1);
@@ -197,7 +204,12 @@ const App = () => {
         setSplitView(false);
       }
     };
-  });
+
+    window.addEventListener('popstate', handlePopState);
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, [setSplitView, updateStateValues]);
 
   return (
     <div id="App" style={{ backgroundColor: colorData1.hex }}>
